5a52690c6e14fdecb23a015496818b8cbbb311f2
adibML007 <a_u_z_ipe@yahoo.com>
2022-12-23 23:51:21 -0500

    first commit

---
+++ pca_selection.py
@@ -0,0 +1,7 @@
+# This function does the selection using principal component analysis (PCA)
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.metrics import classification_report
+
+
+
